a84894759348364784bb085822655205871c8c98
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-11-24 14:04:54 -0800

    connected to database and ready to receieve requests

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
-
 const express = require("express");
 const app = express();
 const mysql = require("mysql")
-
-
+//parsing the connection string
+const {ConnectionString} = require('connection-string'); 
+const dbConnectionSring = new ConnectionString(process.env.CLEARDB_DATABASE_URL)
 const port = process.env.PORT || 8080;
+app.use(express.json());
+
+const dbConnection = mysql.createConnection({
+    host: dbConnectionSring.hosts[0].name,
+    user: dbConnectionSring.user,
+    password: dbConnectionSring.password,
+    database: dbConnectionSring.path[0]
+})
+
+
+
+
+
 
 app.get("/", (req, res)=>{
-    res.send("OK")
+    
+    dbConnection.query('SELECT * FROM product', (error, rows)=>{
+        if(error){
+        res.status(500) 
+        res.send(error)
+        }else{
+            res.send(rows)
+    
+        }
+    })
+   
 })
 
 
@@ -20,8 +43,8 @@ app.get("/", (req, res)=>{
 
 
 
-try{
-    app.listen(port,()=>{
-        console.log(`listening on port ${port}`)
-    })
-}catch(err){console.log(err)}
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+})
+
